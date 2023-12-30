1ff34b2a5338f2a7967e4ea5e291318618781976
chandana24 <gantachandana98@gmail.com>
2023-12-30 16:42:18 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -20,28 +20,28 @@ resource "aws_docdb_subnet_group" "main" {
   tags       = merge(local.tags, { Name = "${local.name_prefix}-subnet-group" })
 }
 
-resource "aws_security_group" "main" {
-  name        = "${local.name_prefix}-sg"
-  description = "${local.name_prefix}-sg"
-  vpc_id      = var.vpc_id
-  tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
+#resource "aws_security_group" "main" {
+#  name        = "${local.name_prefix}-sg"
+#  description = "${local.name_prefix}-sg"
+#  vpc_id      = var.vpc_id
+#  tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
 
-  ingress {
-    description = "DOCDB"
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = var.sg_ingress_cidr
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
+#  ingress {
+#    description = "DOCDB"
+#    from_port   = 27017
+#    to_port     = 27017
+#    protocol    = "tcp"
+#    cidr_blocks = var.sg_ingress_cidr
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#}
 
 resource "aws_docdb_cluster_parameter_group" "main" {
   family      = var.engine_family
